5809b0eb20344b0d2f86d899c5466f84cbcd678d
王佳 <wangjia_1919@163.com>
2019-02-15 17:46:33 +0800

    add config

---
+++ src/main/java/com/nine/cloud/nine/nacos/configserver/NineNacosConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.nine.cloud.nine.nacos.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NineNacosConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NineNacosConfigServerApplication.class, args);
+    }
+
+}
+
