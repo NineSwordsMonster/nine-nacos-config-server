510c9437dd05121ac09bff5fd03c224d4261d0ec
王佳 <wangjia_1919@163.com>
2019-02-15 18:02:34 +0800

    add discovery config

---
+++ src/main/java/com/nine/cloud/nine/nacos/configserver/Config.java
@@ -0,0 +1,15 @@
+package com.nine.cloud.nine.nacos.configserver;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Create by Jarvis.wang on Jarvis's device
+ *
+ * @author Jarvis.wang  Jarvis
+ * @date 2019-02-15 17:54
+ */
+@Configuration
+@RefreshScope
+public class Config {
+}
--- src/main/java/com/nine/cloud/nine/nacos/configserver/NineNacosConfigServerApplication.java
+++ src/main/java/com/nine/cloud/nine/nacos/configserver/NineNacosConfigServerApplication.java
@@ -1,7 +1,11 @@
 package com.nine.cloud.nine.nacos.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class NineNacosConfigServerApplication {
@@ -10,5 +14,19 @@ public class NineNacosConfigServerApplication {
         SpringApplication.run(NineNacosConfigServerApplication.class, args);
     }
 
+
+    @RestController
+    @RequestMapping("/config")
+    @RefreshScope
+    public class ConfigController {
+
+        @Value("${useLocalCache:false}")
+        private boolean useLocalCache;
+
+        @RequestMapping("/get")
+        public boolean get() {
+            return useLocalCache;
+        }
+    }
 }
 
