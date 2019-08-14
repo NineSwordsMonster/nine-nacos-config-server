4a9c38af546742ae66202ac0c4eaea688b613709
王佳 <wangjia_1919@163.com>
2019-08-14 23:30:08 +0800

    refactor

--- src/main/java/com/nine/cloud/nacos/discovery/NineNacosConfigServerApplication.java
+++ src/main/java/com/nine/cloud/nacos/discovery/NineNacosConfigServerApplication.java
@@ -1,11 +1,7 @@
 package com.nine.cloud.nacos.discovery;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class NineNacosConfigServerApplication {
@@ -13,20 +9,4 @@ public class NineNacosConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(NineNacosConfigServerApplication.class, args);
     }
-
-
-    @RestController
-    @RequestMapping("/config")
-    @RefreshScope
-    public class ConfigController {
-
-        @Value("${useLocalCache:false}")
-        private boolean useLocalCache;
-
-        @RequestMapping("/get")
-        public boolean get() {
-            return useLocalCache;
-        }
-    }
 }
-
